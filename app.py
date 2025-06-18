3912fa613f5b33ebc8f76eefab527723f4486ec0
Yk-coding <113650366+Yk-coding@users.noreply.github.com>
2025-06-18 17:20:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
+from io import StringIO
 
 st.title("Labeling UI Prototype")
-st.write("Hello, world")
+uploaded_file = st.file_uploader("Upload an Image", type=["png", "jpg", "jpeg"])
+if uploaded_file is not None:
+    # Display the uploaded image directly
+    st.image(uploaded_file, caption="Uploaded Image", use_container_width=True)
